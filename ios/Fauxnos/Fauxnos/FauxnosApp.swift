6a61ce15b4e0ae19b93d21cb8a55acd55af9575a
dmayman <dmaymandesign@gmail.com>
2025-05-25 10:19:13 -0700

    ios set up

---
+++ ios/Fauxnos/Fauxnos/FauxnosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FauxnosApp.swift
+//  Fauxnos
+//
+//  Created by David Mayman on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FauxnosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
